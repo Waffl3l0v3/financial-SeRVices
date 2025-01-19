1dbafe41785bd0a38b5f4f8af79b4ec9f8c5a562
Vaish1125 <163017961+Vaish1125@users.noreply.github.com>
2025-01-19 22:36:25 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,22 +8,6 @@ const Footer = () => {
       <p className="col-md-4 mb-0 text-light ms-3" style={{ fontFamily: 'Montserrat' }}>
         <i>© 2025 Financial Services, Inc</i>
       </p>
-
-      {/* Navigation links for footer, such as Home, Stock Data, etc. */}
-      <ul className="nav col-md-4 justify-content-end" style={{ fontFamily: 'Montserrat' }}>
-        <li className="nav-item">
-          <a href="/" className="nav-link px-2 text-light">Home</a>
-        </li>
-        <li className="nav-item">
-          <a href="/stockdata" className="nav-link px-2 text-light">Stock Data</a>
-        </li>
-        <li className="nav-item">
-          <a href="/chatbot" className="nav-link px-2 text-light">Chatbot</a>
-        </li>
-        <li className="nav-item">
-          <a href="/resources" className="nav-link px-2 text-light">Resources</a>
-        </li>
-      </ul>
     </footer>
   );
 }
